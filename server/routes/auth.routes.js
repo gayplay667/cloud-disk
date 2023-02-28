1d3df08bb17b770475c670a5e513b44190e4e929
Alisher <alisher.ishangali@fizmat.kz>
2023-02-28 06:40:46 +0300

    lsn8_login_form

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,10 +1,11 @@
 const Router = require('express')
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
-const config = require("config")
-const jwt = require("jsonwebtoken")
+const config = require('config')
+const jwt = require('jsonwebtoken')
 const {check, validationResult} = require('express-validator')
 const router = new Router()
+const authMiddleware = require('../middleware/auth.middleware')
 
 router.post('/registration',
     [
@@ -39,11 +40,11 @@ router.post('/login',
             const {email, password} = req.body
             const user = await User.findOne({email})
             if (!user) {
-                return res.status(404).json({message: `Пользователь не найден`})
+                return res.status(404).json({message: `User not found`})
             }
-            const isPassValid = await bcrypt.compareSync(password, user.password)
-            if(!isPassValid) {
-                return res.status(400).json({message: `Пороль не найден`})
+            const isPassValid = bcrypt.compareSync(password, user.password)
+            if (!isPassValid) {
+                return res.status(400).json({message: `Invalid password`})
             }
             const token = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "1h"})
             return res.json({
@@ -62,5 +63,25 @@ router.post('/login',
         }
     })
 
+router.get('/auth', authMiddleware,
+    async (req, res) => {
+        try {
+            const user = await User.findOne({_id: req.user.id})
+            const token = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "1h"})
+            return res.json({
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    diskSpace: user.diskSpace,
+                    usedSpace: user.usedSpace,
+                    avatar: user.avatar
+                }
+            })
+        } catch (e) {
+            console.log(e)
+            res.send({message: "Server error"})
+        }
+    })
 
 module.exports = router
